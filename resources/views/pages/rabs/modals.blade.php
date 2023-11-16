367fb9b01d8025e10422e91f0a142273acc0a2ab
farhan-hidayat <farhanarchman@gmail.com>
2023-11-16 21:12:16 +0700

    update size modals rab verif

--- resources/views/pages/rabs/modals.blade.php
+++ resources/views/pages/rabs/modals.blade.php
@@ -25,7 +25,7 @@
 @foreach ($rabs as $rab)
     <div class="modal fade" id="ModalVerifikasi{{ $rab->id }}" tabindex="-1" role="dialog"
         aria-labelledby="ModalVerifikasi{{ $rab->id }}" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="ModalVerifikasi{{ $rab->id }}">Verifikasi RAB</h5>
